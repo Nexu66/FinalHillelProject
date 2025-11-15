7c71fb80f33542640a717f1bae7f5c12cb8dd992
Nexus <yaroslaw3718@gmail.com>
2025-11-15 21:18:58 +0200

    incase pc blows up

--- src/core/coreImpl/coreImpl.cpp
+++ src/core/coreImpl/coreImpl.cpp
@@ -2,8 +2,16 @@
 
 namespace Core {
 namespace impl {
-const qsizetype CollatzProcessorImpl::CoresCount = QThread::idealThreadCount();
-const QList<std::jthread> CollatzProcessorImpl::thread_pool{
+const qsizetype CollatzProcessorImpl::s_CoresCount =
+    QThread::idealThreadCount();
+const QList<std::jthread> CollatzProcessorImpl::s_ThreadPool{
     CollatzProcessorImpl::CoresCount};
+const qsizetype s_MaxSize = std::numeric_limits<qsizetype>::max();
+std::atomic<int> CollatzProcessorImpl::element = 0;
+
+std::pair<int, int> CollatzProcessorImpl::StartProcessing(
+    const qsizetype CurrentUpperLimit, const qsizetype CurrentThreadLimit) {}
+for (int i = 0; i < CurrentThreadLimit; ++i) {
+}
 }  // namespace impl
 }  // namespace Core
--- src/core/coreImpl/coreImpl.hpp
+++ src/core/coreImpl/coreImpl.hpp
@@ -1,14 +1,20 @@
 #pragma once
 #include <QList>
 #include <QThread>
+#include <atomic>
+#include <limits>
 #include <thread>
 
 namespace Core {
 namespace impl {
 class CollatzProcessorImpl {
  public:
-  static const qsizetype CoresCount;
-  static const QList<std::jthread> thread_pool;
+  static const qsizetype s_CoresCount;
+  static const QList<std::jthread> s_ThreadPool;
+  static const qsizetype s_MaxSize;
+
+  std::pair<int, int> StartProcessing(const qsizetype CurrentUpperLimit,
+                                      const qsizetype CurrentThreadLimit);
 };
 
 }  // namespace impl
